0a8c8c6de1f8a17b4bf7fb78cfd6733aa526dc8e
Jesús Germade <jesus@germade.es>
2018-08-27 12:46:44 +0200

    better eval ReferenceError behavior

--- eval.js
+++ eval.js
@@ -1,9 +1,14 @@
 
+function _getSymbol (err) {
+  if( /:/.test(err.message) ) return err.message.split(':').pop();
+  return err.message.split(/ +/)[0];
+}
+
 module.exports = function _evalExpression (expression, _scope) {
   if( typeof expression !== 'string' ) throw new TypeError('expression should be a String');
-  
+
   var execExpression = new Function('scope', 'with(scope) { return (' + expression + '); };');
-   
+
    function evalGetter (scope) {
     scope = scope || {};
     try {
@@ -12,7 +17,7 @@ module.exports = function _evalExpression (expression, _scope) {
       // console.log('ReferenceError', err, err instanceof ReferenceError);
       if( err instanceof ReferenceError ) {
         scope = Object.create(scope);
-        scope[err.message.substr(0, err.message.length - 15) ] = null;
+        scope[_getSymbol(err)] = null;
         return execExpression(scope);
       }
       return '';
