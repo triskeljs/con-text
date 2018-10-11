089b2f1c8528a7d1c0d8edf49397c871ab8098d4
Jesús Germade <jesus@germade.es>
2018-10-11 17:56:11 +0200

    evalExpression without with{...}

--- eval.js
+++ eval.js
@@ -1,30 +1,61 @@
 
-function _getSymbol (err) {
-  if( /:/.test(err.message) ) return err.message.split(/ *: */).pop().split(/ +/)[0];
-  return (err.message.split(/ +/)[0] || '').replace(/^'|'$/g, '');
-}
+function _evalExpression (expression) {
+  var matches = [],
+      valid_keys = {};
 
-module.exports = function _evalExpression (expression, _scope) {
   if( typeof expression !== 'string' ) throw new TypeError('expression should be a String');
 
-  var execExpression = new Function('scope', 'with(scope) { return (' + expression + '); };');
+  (expression.replace(/'[^']'|"[^"]/g, '').match(/\.?[a-zA-Z]\w+/g) ||[]).forEach(function (key) {
+    if( key[0] === '.' || valid_keys[key] ) return;
 
-   function evalGetter (scope) {
-    scope = scope || {};
-    try {
-      return execExpression(scope);
-    } catch(err) {
-      // console.log('ReferenceError', err, err instanceof ReferenceError);
-      if( err instanceof ReferenceError ) {
-        scope = Object.create(scope);
-        scope[_getSymbol(err)] = null;
-        return execExpression(scope);
-      }
-      return '';
-    }
-  }
+    valid_keys[key] = true;
 
-  if( _scope === undefined ) return evalGetter;
+    matches.push(key);
+  });
 
-  return evalGetter(_scope);
+  valid_keys = null;
+
+  var runExpression = Function.apply(null, matches.concat('return (' + expression + ');') );
+
+  return function (scope) {
+    if( !scope ) scope = {};
+
+    return runExpression.apply(null, matches.map(function (key) { return scope[key]; }) );
+  };
+}
+
+module.exports = function (expression, scope) {
+  if( scope === undefined ) return _evalExpression(expression);
+
+  return _evalExpression(expression)(scope);
 };
+
+// function _getSymbol (err) {
+//   if( /:/.test(err.message) ) return err.message.split(/ *: */).pop().split(/ +/)[0];
+//   return (err.message.split(/ +/)[0] || '').replace(/^'|'$/g, '');
+// }
+//
+// module.exports = function _evalExpression (expression, _scope) {
+//   if( typeof expression !== 'string' ) throw new TypeError('expression should be a String');
+//
+//   var execExpression = new Function('scope', 'with(scope) { return (' + expression + '); };');
+//
+//    function evalGetter (scope) {
+//     scope = scope || {};
+//     try {
+//       return execExpression(scope);
+//     } catch(err) {
+//       // console.log('ReferenceError', err, err instanceof ReferenceError);
+//       if( err instanceof ReferenceError ) {
+//         scope = Object.create(scope);
+//         scope[_getSymbol(err)] = null;
+//         return execExpression(scope);
+//       }
+//       return '';
+//     }
+//   }
+//
+//   if( _scope === undefined ) return evalGetter;
+//
+//   return evalGetter(_scope);
+// };
