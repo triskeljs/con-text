874b3461ba3eccf457fce2c998ed623620708b3e
Jesús Germade <jesus@germade.es>
2018-11-22 02:42:03 +0100

    Added null,true,false,undefined,arguments keywords

--- eval.js
+++ eval.js
@@ -3,7 +3,7 @@
 
 var ecma_keywords = {};
 
-'break,case,catch,class,const,continue,debugger,default,delete,do,else,export,extends,finally,for,function,if,import,in,instanceof,new,return,super,switch,this,throw,try,typeof,var,void,while,with,yield'.split(',').forEach(function (key) {
+'null,true,false,undefined,arguments,break,case,catch,class,const,continue,debugger,default,delete,do,else,export,extends,finally,for,function,if,import,in,instanceof,new,return,super,switch,this,throw,try,typeof,var,void,while,with,yield'.split(',').forEach(function (key) {
   ecma_keywords[key] = true;
 });
 
